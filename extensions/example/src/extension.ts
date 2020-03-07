4c83af02616457a06d40f441632aa7cce0f8cdac
liuxiong.lx <liuxiong.lx@alibaba-inc.com>
2020-03-07 18:47:12 +0800

    chore: try tag

--- extensions/example/src/extension.ts
+++ extensions/example/src/extension.ts
@@ -4,6 +4,7 @@ export function activate(context: vscode.ExtensionContext): void {
   context.subscriptions.push(vscode.commands.registerCommand('iceworks.start', function() {
     vscode.window.showInformationMessage('start iceworks!');
   }));
+
   context.subscriptions.push(vscode.commands.registerCommand('iceworks.build', function() {
     vscode.window.showInformationMessage('build iceworks!');
   }));
