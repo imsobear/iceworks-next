dc6c4855221df4074bf488bfd1b4a1410e4d9456
noyobo <noyobo@gmail.com>
2020-03-08 17:57:47 +0800

    feat: 飞冰物料展示

--- app/src/routes.ts
+++ app/src/routes.ts
@@ -1,8 +1,21 @@
-
 import Home from '@/pages/Home';
+import Layout from '@/layouts/Layout';
 
-export default [{
-  path: '/',
-  exact: true,
-  component: Home,
-}];
+export default [
+  {
+    path: '/',
+    component: Layout,
+    children: [
+      {
+        path: '/home',
+        exact: true,
+        component: Home
+      },
+      {
+        path: '/',
+        // 重定向
+        redirect: '/home'
+      }
+    ]
+  }
+];
